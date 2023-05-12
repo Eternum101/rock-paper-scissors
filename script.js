d57ba73a128412566ba8f5bb0223dd06d6a0abb4
Eternum101 <eternumhd111@gmail.com>
2023-05-12 20:03:21 +1000

    Removed Comments in script.js

--- script.js
+++ script.js
@@ -124,37 +124,3 @@ function resetGame() {
         window.location.reload();
     });
 }
-
-// If playerScoreSpan > computer score {
-    "You won! Congragulations!" 
-//} else {
-    "You lost! :("
-//}
-
-//function playRound(playerSelection, computerSelection) {
-
-//    if (playerSelection === "Rock" && computerSelection === "Paper") {
-//        return "You lose! Paper beats Rock!"; 
-//    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-//        return "You win! Paper beats Rock!";
-//    } else if (playerSelection === "Scissors" ** computerSelection === "Rock") {
-//        return "You lose! Rock beats Scissors!"; 
-//    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-//        return "You win! Rock beats Scissors!";
-//    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-//        return "You lose! Scissors beats Paper! ";
-//    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-//        return "You win! Scissors beats Paper!";
-//    } else if (playerSelection === computerSelection) {
-//        return "Draw"; 
-//    }
-//}
-
-//function game() {
-//    for (let i = 0; i < 5; i++) {
-//        let playerSelection = prompt("Enter Rock, Paper or Scissors");
-//        let computerSelection = getComputerChoice();
-//        console.log(playRound(playerSelection, computerSelection));
-//    }
-//}
-//game();
